b2309fe1acde92cf15d2bfcb5f198edaf9facf15
raquibbi <raquibbi@gmail.com>
2019-12-16 01:32:57 +0000

    added front page last person to update

--- app/controllers/kibis_controller.rb
+++ app/controllers/kibis_controller.rb
@@ -8,4 +8,18 @@ class KibisController < ApplicationController
     @kibi = Kibi.new
   end
 
+  def create
+    @kibi = Kibi.create(kibi_params)
+    if @kibi.invalid?
+      flash[:error] = '<strong> you didnt complete all fields!!</strong>'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def kibi_params
+    params.require(:kibi).permit(:name, :description, :address)
+  end
+
 end
