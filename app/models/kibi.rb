a75d6da019397e4c4db6557d94216d658c8db951
raquibbi <raquibbi@gmail.com>
2019-12-16 01:43:33 +0000

    added validation for kibi form

--- app/models/kibi.rb
+++ app/models/kibi.rb
@@ -1,2 +1,5 @@
 class Kibi < ApplicationRecord
+  validates :description, presence: true, length: { maximum: 140, minimum: 1 }
+  validates :name, presence: true, length: { maximum: 50, minimum: 1 }
+  validates :address, presence: true, length: { maximum: 50, minimum: 1 }
 end
